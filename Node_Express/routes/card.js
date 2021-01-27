ed21b871d6f3fa99371abbaba022000bfbcb5955
Yevhenii Silin <zhenya.sil96@gmail.com>
2021-01-27 19:08:13 +0200

    cart display

--- Node_Express/routes/card.js
+++ Node_Express/routes/card.js
@@ -3,6 +3,18 @@ const router = Router()
 //const Card = require('../models/card')
 const Course = require('../models/course')
 
+function mapCartItems(cart) {
+  return cart.items.map(c => ({
+    ...c.courseId._doc, count: c.count
+  }))
+}
+
+function computePrice(courses) {
+  return courses.reduce((total, course) => {
+    return total += course.price * course.count
+  }, 0)
+}
+
 router.post('/add', async (req, res) => {
   const course = await Course.findById(req.body.id)
   //await Card.add(course)
@@ -11,12 +23,18 @@ router.post('/add', async (req, res) => {
 })
 
 router.get('/', async (req, res) => {
-    const card = await Card.fetch()
+    //const card = await Card.fetch()
+    const user = await req.user
+    .populate('cart.items.courseId')
+    .execPopulate('')
+
+    const courses = mapCartItems(user.cart)
+
     res.render('card', {
         title: 'Корзина',
         isCard: true,
-        courses: card.courses,
-        price: card.price
+        courses: courses,
+        price: computePrice(courses)
     })
 })
 
