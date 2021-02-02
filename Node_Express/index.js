64c9d117dced15cd7f5a8dc7d523888a1a308cc3
Yevhenii Silin <zhenya.sil96@gmail.com>
2021-02-02 23:03:31 +0200

    Add session

--- Node_Express/index.js
+++ Node_Express/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
 const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
 const ordersRoutes = require('./routes/orders')
@@ -9,6 +10,8 @@ const addRoutes = require('./routes/add')
 const authRoutes = require('./routes/auth')
 const coursesRoutes = require('./routes/courses')
 const User = require('./models/user')
+const varMiddleware = require('./middleware/variables')
+
 
 
 const app = express()
@@ -34,6 +37,13 @@ app.use(async (req, res, next) => {
 
 app.use(express.static(path.join(__dirname,'public')))
 app.use(express.urlencoded({extended: true}))
+app.use(session({
+  secret: 'some secret value',
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(varMiddleware)
 
 app.use('/', homeRoutes)
 app.use('/add', addRoutes)
