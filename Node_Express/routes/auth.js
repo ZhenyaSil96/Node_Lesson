2055c4a695b41155e30cbb4dfe0845247173f8d7
Yevhenii Silin <zhenya.sil96@gmail.com>
2021-02-03 17:21:27 +0200

    login user

--- Node_Express/routes/auth.js
+++ Node_Express/routes/auth.js
@@ -19,16 +19,32 @@ router.get('/logout', async(req, res) => {
   })
 
 router.post('/login', async(req, res) => {
-  const user = await User.findById('60116d4d1e3b9e0df019eae1')
-  req.session.user = user
-  req.session.isAuthenticated = true
-  req.session.save(err =>{
-      if(err){
-          throw err
+  try{
+   const {email, password} = req.body
+   const candidate = await User.findOne({email})
+
+   if(candidate) {
+      const areSame = password === candidate.password
+
+      if(areSame) {
+        const user = candidate             //await User.findById('60116d4d1e3b9e0df019eae1')
+        req.session.user = user
+        req.session.isAuthenticated = true
+        req.session.save(err =>{
+            if(err){
+                throw err
+            }
+            res.redirect('/')
+        })
+      }else{
+        res.redirect('/auth/login#login')
       }
-      res.redirect('/')
-  })
- 
+   }else {
+      res.redirect('/auth/login#login')
+   }
+  }catch(e){
+    console.log(e)
+  }
 })
 
 router.post('/register', async (req, res) => {
