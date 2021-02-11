4121eea2be9891755032c552b41988db8f6cf734
Yevhenii Silin <zhenya.sil96@gmail.com>
2021-02-11 12:42:39 +0200

    key protection

--- Node_Express/keys/index.js
+++ Node_Express/keys/index.js
@@ -1,7 +1,5 @@
-module.exports = {
-    MONGODB_URI:'mongodb+srv://Yevhenii:69jYDFauqgXAMDN@cluster0.j89wu.mongodb.net/shop',
-    SESSION_SECRET: 'some secret value',
-    SENDGRID_API_KEY: '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',
-    ENAIL_FROM: 'nodejsStarts.96@gmail.com',
-    BASE_URL: 'http://localhost:3000'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys.prod')
+}else{
+  module.exports = require('./keys.dev')
 }
---
+++ Node_Express/keys/keys.dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MONGODB_URI:'mongodb+srv://Yevhenii:69jYDFauqgXAMDN@cluster0.j89wu.mongodb.net/shop',
+    SESSION_SECRET: 'some secret value',
+    SENDGRID_API_KEY: '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',
+    ENAIL_FROM: 'nodejsStarts.96@gmail.com',
+    BASE_URL: 'http://localhost:3000'
+}
