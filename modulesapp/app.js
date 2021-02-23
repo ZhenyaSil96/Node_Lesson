9bfe178c12cde89974e788fefe58b10d183b11f3
Yevhenii Silin <zhenya.sil96@gmail.com>
2021-02-23 16:22:46 +0200

    read file and write file

--- modulesapp/app.js
+++ modulesapp/app.js
@@ -2,6 +2,8 @@ const http = require('http')
 const express = require('express') // Получаем модуль express
 const server = express() // создаем приложение
 
+const fs = require('fs')
+
 ///////////////////////////////////////////////
 // app.get('/', function(req, res) { //Установаливаем обработчик для маршурта 
 //  // res.end('Hello from Express')
@@ -40,22 +42,53 @@ server.listen(3000)
 
 ///////////////////////////////////////////
 
-function displaySync (callback){
-  callback()
-}
+// function displaySync (callback){
+//   callback()
+// }
 
-console.log('Начало работы')
+// console.log('Начало работы')
 
-setTimeout(() => {
-    console.log('timeout 500')
-},500)
+// setTimeout(() => {
+//     console.log('timeout 500')
+// },500)
 
-setTimeout(() => {
-   console.log('timeout 100')
-}, 100)
+// setTimeout(() => {
+//    console.log('timeout 100')
+// }, 100)
 
-displaySync(() => {
-    console.log('withhout time')
-})
-console.log('Завершение программы')
+// displaySync(() => {
+//     console.log('withhout time')
+// })
+// console.log('Завершение программы')
 
+///////////Синхронное чтение из файла////////////
+console.log('Синхронное чтение из файла')
+let file = fs.readFileSync('file.txt', 'utf8')
+console.log(file)
+///////////////////////////////////////////////
+
+
+//Асинхронное чтение из файла
+
+let fileH = fs.readFile('h.txt', 'utf8', 
+    function (err, data) {
+        console.log('Асинхронное чтение из файлов ')
+        if (err) throw console.log(`Error: ${err}`)
+        // Чтение из файла
+        console.log(data)
+    })
+//////////////////////////////////////////////////
+
+//////Запись в файл ///////////////////////////////
+
+let fileWriteFile = fs.writeFile('file.txt','Метод создает строку URL запроса от заданного obj перебирая «собственных свойств» объекта.',
+                        function(err) {
+                            if (err) throw err
+                            console.log('Асинхронная запись в файл')
+                            let data1 = fs.readFileSync('file.txt')
+                            console.log(data1)
+                        })
+
+fileWriteFile = fs.appendFileSync('file.txt', 'Метод добавления appendFileSync')
+fileWriteFile = fs.readFileSync('file.txt', 'utf8')
+console.log(fileWriteFile)
